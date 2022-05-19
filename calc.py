6755d4606330b0f11ecd28d8424680b580f05b3c
lemmy2030 <jacklemmy@gmail.com>
2022-05-19 03:51:34 -0700

    strings in python

---
+++ calc.py
@@ -0,0 +1,5 @@
+f_num = input("enter the first number :")
+s_num = input("enter the second number :")
+int(f_num) + int(s_num)
+sum_of_nums = f_num + s_num
+print("the sum of the two numbers is " +(int(sum_of_nums)))
---
+++ strings.py
@@ -0,0 +1,34 @@
+from socket import J1939_NLA_BYTES_ACKED
+
+
+name = "lemmy"
+
+#multiple strings
+msg="""qrs3x6 cconfirmed you have received ksh 12000 from jack lemmy safaricom cares for you"""
+print(msg)
+city = "nairobi"
+# .upper() to convert to upper case
+print(city.upper())
+uni = "JKUAT"
+print(uni.lower())
+fruit = "pinneaple"
+print(fruit[:2])
+print(fruit[-1:])
+print(fruit[5:])
+#strip removes spaces between the words
+f_name = " Bob afwata"
+print(f_name.strip())
+f_name = "lemmy"
+
+s_name = "Jack"
+print(full_name)
+city = "Mombasa"
+print(city.len())
+print(len(city))
+word = "deoxyrebonucleicacid"
+print(word.len())
+x = 100
+print(str(x))
+
+x = 100
+print(str(x))
