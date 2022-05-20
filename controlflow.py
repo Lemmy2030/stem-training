123ad45cfcecd8f89a6459bd926214dbee107b0f
lemmy2030 <jacklemmy641@gmail.com>
2022-05-20 00:41:14 -0700

    strings in python

---
+++ controlflow.py
@@ -0,0 +1,27 @@
+from re import X
+
+
+print("Done")
+
+
+##else statement
+x = 2
+if x == 10:
+    print(X)
+else :
+    print("Not 10")
+## if statement
+x = 4
+if x > = 1 :
+    print("Hey i'm still here")
+    x == 1
+    print(x)
+    #elif to create grading system
+    grade = int(input ("write student score: "))
+    if grade >= 80 and grade <= 100:
+        print("student got an A")
+    elif grade >= 70 and grade < 80:
+        print("student got a B")
+     elif grade >=60 and grade < 70:
+            
+
---
+++ lists.py
@@ -0,0 +1,31 @@
+##lists
+words =["apples","love","people," "!"]
+print (words[0])
+print (words[1])
+print (words)
+#lists can store any data types
+dat = ['a,'"!","too",'5','4',"hey"]
+
+#nested lists.
+m = [
+    [5,6,7],
+    [8,9,1]
+    ]
+print (m)
+str = "Hello class"
+print(str[5])
+print(str[6])
+print(str[7-3])
+
+strg = ["Hello ","123","51","abc","Hey","b","a"]
+print(strg)
+print(strg[6])
+print(strg[5])
+print(strg[7-3])
+strg[0] = strg[7-2]
+print(strg)
+
+#lists index can be readressed
+subjects = ["math","science","religious"]
+subjects[2] = "mechanics"
+print(subjects)
